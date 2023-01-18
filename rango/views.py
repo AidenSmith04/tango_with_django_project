3d790bbea2d267c92e176039741568d7ad02c1bc
Aiden Smith <2660428S@student.gla.ac.uk>
2023-01-18 15:52:15 +0000

    Chapter 10 Done

--- rango/views.py
+++ rango/views.py
@@ -23,14 +23,17 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
 
-    response = render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request)
 
-    visitor_cookie_handler(request, response)
+    response = render(request, 'rango/index.html', context=context_dict)
 
     return response
 
 def about(request):
     context_dict = {'boldmessage': 'This tutorial has been put together by Aiden Smith'}
+
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
 
     return render(request, 'rango/about.html', context=context_dict)
 
@@ -155,16 +158,22 @@ def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
 
-def visitor_cookie_handler(request, response):
-    visits = int(request.COOKIES.get('visits', '1'))
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
 
-    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
     last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
 
-    if (datetime.now() - last_visit_time).days >0:
+    if (datetime.now() - last_visit_time).days > 0:
         visits = visits + 1
-        response.set_cookie('last_visit', str(datetime.now()))
+        request.session['last_visit'] = str(datetime.now())
     else:
-        response.set_cookie('last_visit', last_visit_cookie)
+        request.session['last_visit'] = last_visit_cookie
 
-    response.set_cookie('visits', visits)
+    request.session['visits'] =  visits
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
